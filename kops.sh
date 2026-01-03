3b42878bf03eb50ed2fe0039070ea961d54be7d7
Prasanth Madullapalle <mpgc922@gmail.com>
2026-01-03 16:30:01 +0530

    Add files via upload

--- kops.sh
+++ kops.sh
@@ -1,18 +1,15 @@
-#vim .bashrc
-#export PATH=$PATH:/usr/local/bin/
-#source .bashrc
-
-
 #! /bin/bash
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.33.0/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
+echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
-aws s3api create-bucket --bucket rahamssshaik09.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket rahamssshaik09.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://rahamssshaik09.k8s.local
-kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name rahams.k8s.local --yes --admin
+aws s3api create-bucket --bucket mpgc922.k8s.local --region ap-south-1  --create-bucket-configuration LocationConstraint=ap-south-1
+aws s3api put-bucket-versioning --bucket mpgc922.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://mpgc922.k8s.local
+kops create cluster --name mpgc922.k8s.local --zones ap-south-1a --control-plane-count=1 --control-plane-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name mpgc922.k8s.local --yes --admin
+
